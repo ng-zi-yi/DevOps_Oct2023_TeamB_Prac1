0d2fea9e83d04a7fb11fc125e4681cfc6dbc776e
ng-zi-yi <s10222211@connect.np.edu.sg>
2023-11-20 15:46:28 +0800

    feat: added search for course fees

---
+++ features/searchBarQuery.feature
@@ -0,0 +1,9 @@
+Feature: Search for Course Fees
+    Scenario: Search for "fees"
+        Given Chrome browser is Launched
+        When Open NP Website
+        Then Select navigation bar search button
+        Then Input search query "fees"
+        Then Select form search button
+        Then Select course fees search result
+        And Close browser
--- features/steps/testfile.py
+++ features/steps/testfile.py
@@ -1,3 +1,46 @@
 from behave import *
 from selenium import webdriver
 from time import sleep
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.select import Select
+
+@given(u'Chrome browser is Launched')
+def laucnhChromeBrowser(context):
+    context.driver = webdriver.Chrome()
+
+@when(u'Open NP Website')
+def openNPPage(context):
+    context.driver.get('https://www.np.edu.sg/home')
+
+@then(u'Select navigation bar search button')
+def navSearch(context):
+    context.driver.find_element(By.XPATH,"//*[@id='navbarNavDropdown']/ul/li[8]/button/em").click()
+
+@then(u'Input search query "fees"')
+def inputQuery(context):
+    context.driver.find_element(By.XPATH,"//*[@id='fsearch']").send_keys("fees")
+
+@then(u'Select form search button')
+def clickSearch(context):
+    context.driver.find_element(By.XPATH,"/html/body/div[2]/div/div/div[1]/div/button/em").click()
+    sleep(5)
+    context.driver.switch_to.window(context.driver.window_handles[1])
+
+
+@then(u'Select course fees search result')
+def clickCourseFees(context):
+    context.driver.find_element(By.XPATH,"//*[@id='sitesearch-result']/div[2]/a[1]").click()
+    sleep(5)
+
+
+@then(u'Verify if able to Search')
+def verifySearch(context):
+    context.driver.find_element(By.XPATH,"/html/body/main/section/div/div[2]/div[2]/div")
+    sleep(2)
+    
+
+@then(u'Close browser')
+def closeBrowser(context):
+    context.driver.close()
+
+
